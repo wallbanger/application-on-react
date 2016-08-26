a32efe4ea5c7cebc75b67144465ad86a88b8f675
Eugene Koval <iam.eugenekoval@gmail.com>
2016-08-26 17:08:59 +0300

    proccesing moves to flux

---
+++ src/js/pages/Todos.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Todo from '../components/Todo'
+
+export default class Featured extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          id: 1234342,
+          text: "go to shop",
+          complete: false,
+        },
+        {
+          id: 21726353,
+          text: "pay pizza",
+          complete: false,
+        }
+      ]
+    }
+  }
+  render() {
+    const { todos } = this.state;
+    const TodoComponents = todos.map((todo => {
+      return <Todo key={todo.id} {...todo} />
+    }))
+    return(
+      <div>
+        <h1>Todos</h1>
+        <ul>{TodoComponents}</ul>
+      </div>
+    );
+  }
+}
