5598da9f3a3c4fc59028fd3f64f45dcf1e92e258
Eugene Koval <iam.eugenekoval@gmail.com>
2016-08-31 00:44:18 +0300

    add dispatcher create_todo

--- src/js/stores/TodoStore.js
+++ src/js/stores/TodoStore.js
@@ -1,4 +1,5 @@
 import { EventEmitter } from 'events';
+import dispatcher from '../dispatcher';
 
 class TodoStore extends EventEmitter {
   constructor() {
@@ -38,18 +39,29 @@ class TodoStore extends EventEmitter {
     this.todos.push({
       id,
       text,
-      complete: false
+      complete: false,
     });
 
-    this.emit('change');
+    this.emit("change");
   }
 
   getAll() {
     return this.todos;
   }
 
+  handleActions(action) {
+    switch(action.type) {
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+        break;
+      }
+    }
+  }
+
 }
 
 const todoStore = new TodoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore));
+window.dispatcher = dispatcher;
 
 export default todoStore;
